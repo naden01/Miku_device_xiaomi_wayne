5e30075f56a921f1e4b5385d646db57e4548c771
Ginger Cat <gingercat@DESKTOP-7C1OONH.localdomain>
2025-11-16 15:22:06 +0300

    Remove conflicting libhidlbase-v32.so VNDK copy
    This library is now provided by Soong modules and doesn't need

--- device.mk
+++ device.mk
@@ -439,7 +439,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+#     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
