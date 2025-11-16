a37240599f09d38e62893c995f9a816bb7dce8e0
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-11-16 03:49:57 -0800

    fix build error

--- device.mk
+++ device.mk
@@ -429,9 +429,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SHIPPING_API_LEVEL := 27
 
 # Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+#PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+#PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # USB
 PRODUCT_PACKAGES += \
